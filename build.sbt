9b7590b515d53aa009963df4f7646089ad0e97d4
Ed Sweeney <ed@onextent.com>
2020-09-11 14:56:24 -0700

    rev201

--- build.sbt
+++ build.sbt
@@ -11,8 +11,8 @@ parallelExecution in test := false
 
 version := "1.0"
 
-scalaVersion := "2.12.6"
-val akkaVersion = "2.5.31"
+scalaVersion := "2.12.12"
+val akkaVersion = "2.6.9"
 
 libraryDependencies ++=
   Seq(
@@ -23,7 +23,7 @@ libraryDependencies ++=
     "ch.qos.logback" % "logback-classic" % "1.1.11",
     "com.typesafe.akka" %% "akka-actor" % akkaVersion,
     "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-    "com.lightbend.akka" %% "akka-stream-alpakka-mqtt" % "0.20",
+    "com.lightbend.akka" %% "akka-stream-alpakka-mqtt" % "2.0.1",
     "org.scalatest" %% "scalatest" % "3.0.9" % "test"
 
   )
--- src/main/scala/somind/dtlab/ingest/mqtt/Main.scala
+++ src/main/scala/somind/dtlab/ingest/mqtt/Main.scala
@@ -1,10 +1,27 @@
 package somind.dtlab.ingest.mqtt
 
+import akka.Done
 import com.typesafe.scalalogging.LazyLogging
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+import Conf._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App with LazyLogging {
 
-  //SubActor()
-  Stream()
+  def handleTerminate(result: Future[Done]): Unit = {
+    result onComplete {
+      case Success(_) =>
+        logger.warn("success. but stream should not end!")
+        actorSystem.terminate()
+      case Failure(e) =>
+        logger.error(s"failure. stream should not end! $e", e)
+        actorSystem.terminate()
+    }
+  }
+
+  handleTerminate(Stream())
 
 }
